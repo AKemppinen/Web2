61cd19fe8af3d00bab393a1f73e2781e2dfc0fad
Antti <anttkem@metropolia.fi>
2023-03-01 10:38:48 +0200

    vue post

--- vueroutermenu-master/src/components/AddEvent.vue
+++ vueroutermenu-master/src/components/AddEvent.vue
@@ -1,11 +1,47 @@
 <template>
-    <p class="homeText">
+    <div id="app">
+        <p class="homeText">
         Syötä tapahtuma
-    </p>
+        </p>
+        <br>
+        <input v-model="eventName" type="text" /><br />
+        <br>
+        <date-picker v-model="eventDate" type="datetime"></date-picker>
+        <br>
+        <br>
+        <input v-model="eventPlace" type="text" /><br />
+        <br>
+        <button @click="addEvent()">Add Event</button>
+  </div>
 </template>
 <script>
-    export default {
-    name: 'AddEvent'
+import axios from "axios";
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+export default {
+  name: "App",
+  components: { DatePicker },
+  data() {
+    return {
+      events: [],
+      eventName: "",
+      eventDate: null,
+      eventPlace: "",
+    };
+  },
+  methods: {
+    async addEvent() {
+      const res = await axios.post(`http://localhost:3001/events`, {
+        name: this.eventName,
+        date: this.eventDate,
+        place: this.eventPlace,
+      });
+      this.events = [...this.events, res.data];
+      this.eventName = "";
+      this.eventDate = "";
+      this.eventPlace = "";
+    },
+  },
 };
 </script>
 <style scoped>
