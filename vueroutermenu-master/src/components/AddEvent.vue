fb5343d4f8cdf4afd80f76290f079285689631e7
Antti <anttkem@metropolia.fi>
2023-03-08 10:42:18 +0200

    vuerouter

--- vueroutermenu-master/src/components/AddEvent.vue
+++ vueroutermenu-master/src/components/AddEvent.vue
@@ -1,18 +1,20 @@
 <template>
+  <form>
     <div id="app">
-        <p class="homeText">
+      <p class="homeText">
         Syötä tapahtuma
-        </p>
-        <br>
-        <input v-model="eventName" type="text" /><br />
-        <br>
-        <date-picker v-model="eventDate" type="datetime"></date-picker>
-        <br>
-        <br>
-        <input v-model="eventPlace" type="text" /><br />
-        <br>
-        <button @click="addEvent()">Add Event</button>
-  </div>
+      </p>
+      <br>
+      <input id="textbox" v-model="eventName" type="text" required /><br />
+      <br>
+      <date-picker v-model="eventDate" type="datetime"></date-picker>
+      <br>
+      <br>
+      <input v-model="eventPlace" type="text" required /><br />
+      <br>
+      <button @click="addEvent()">Add Event</button>
+    </div>
+  </form>
 </template>
 <script>
 import axios from "axios";
@@ -31,26 +33,47 @@ export default {
   },
   methods: {
     async addEvent() {
+
+      if (!this.eventDate || !this.eventName || !this.eventPlace) {
+        alert('Please fill in the required information');
+        return;
+      }
+      if (this.eventDate && this.eventName && this.eventPlace) {
+        alert('Tiedot lähetetty');
+      }
+
       const res = await axios.post(`http://localhost:3001/events`, {
         name: this.eventName,
         date: this.eventDate,
         place: this.eventPlace,
       });
+
       this.events = [...this.events, res.data];
       this.eventName = "";
       this.eventDate = "";
       this.eventPlace = "";
+
     },
   },
+
+  mounted: function () {
+  this.$nextTick(function () {
+    // Code that will run only after the
+    // entire view has been rendered
+    const textbox = document.getElementById('textbox');
+    textbox.focus();
+  })
+}
+
 };
 </script>
 <style scoped>
-    .homeText{
-    font-size: 35px;
-    color: red;
-    text-align: center;
-    position: relative;
-    top:30px;
-    text-shadow: 2px 2px 2px gray;
+.homeText {
+  font-size: 35px;
+  color: red;
+  text-align: center;
+  position: relative;
+  top: 30px;
+  text-shadow: 2px 2px 2px gray;
 }
 </style>
